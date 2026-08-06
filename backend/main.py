3fbead5743433d67a3f055719394997fa03b3d36
BananazrCRAZY <76630376+BananazrCRAZY@users.noreply.github.com>
2026-08-06 00:30:01 -0700

    fetching

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hello BrewFinder!"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/coffee-shops")
+def get_coffee_shops():
+    return [
+    {
+        "id": 1,
+        "name": "Arcade Coffee Roasters",
+        "city": "Riverside"
+    },
+    {
+        "id": 2,
+        "name": "Condron Coffee",
+        "city": "Riverside"
+    }
+]
