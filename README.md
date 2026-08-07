b0dbc8005d6867c316b718bbd934467d10e5e03b
BananazrCRAZY <76630376+BananazrCRAZY@users.noreply.github.com>
2026-08-06 21:17:22 -0700

    frontend backend connected

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ to activate python environment in backend
 venv\Scripts\activate
 uvicorn main:app --reload
 localhost:8000
+
+for react page in frontend
+npm run dev
+http://localhost:5173
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from "react";
 import './App.css'
+
+interface CoffeShop {
+  id: number;
+  name: string;
+  city: string;
+}
 
 function App() {
 
+  const [coffeeShops, setCoffeeShops] = useState<CoffeeShop[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/coffee-shops")
+      .then((response) => response.json())
+      .then((data) => {
+        setCoffeeShops(data);
+      });
+  }, []);
+
   return (
     <>
-      
+      <h1>VibeSips</h1>
+
+      <h2>Coffee Shops</h2>
+
+      {coffeeShops.map((shop) => (
+        <div key = {shop.id}>
+          <h3>{shop.name}</h3>
+          <p>{shop.city}</p>
+        </div>
+      ))}
     </>
   )
 }
